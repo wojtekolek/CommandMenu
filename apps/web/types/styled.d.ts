21e1a8d2f4261be74d240d1787b9040f8106956b
wojtekolek <wojtek_olek@icloud.com>
2022-09-02 12:42:58 +0200

    Add styles stuff

---
+++ apps/web/types/styled.d.ts
@@ -0,0 +1,8 @@
+import 'styled-components'
+
+import { theme } from 'utils/styles/theme'
+
+declare module 'styled-components' {
+  type Theme = typeof theme
+  export interface DefaultTheme extends Theme {}
+}
---
+++ apps/web/utils/styles/theme.ts
@@ -0,0 +1,11 @@
+import { boxShadows, colors, fontSize, fontWeight, radius, spacing, zIndex } from './variables'
+
+export const theme = {
+  colors,
+  fontSize,
+  zIndex,
+  spacing,
+  radius,
+  boxShadows,
+  fontWeight
+}
