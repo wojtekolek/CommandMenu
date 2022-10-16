dabbc82a5921a0a602b6c48375a586fe83449699
wojtekolek <wojtek_olek@icloud.com>
2022-10-16 22:48:46 +0200

    Add Section

---
+++ apps/example/components/Primitives/Section.tsx
@@ -0,0 +1,31 @@
+import type { FunctionComponent, ReactNode } from 'react'
+import { useRef } from 'react'
+
+import { motion, useScroll, useTransform } from 'framer-motion'
+import styled from 'styled-components'
+
+const AnimatedSectionWrapper = styled(motion.div)`
+  position: relative;
+`
+
+type AnimatedSectionProps = {
+  className?: string
+  children: ReactNode
+  id: string
+}
+
+export const AnimatedSection: FunctionComponent<AnimatedSectionProps> = ({
+  className,
+  children,
+  id
+}) => {
+  const ref = useRef(null)
+  const { scrollYProgress } = useScroll({ target: ref, offset: ['24px end', '192px end'] })
+  const opacity = useTransform(scrollYProgress, [0, 1], [0, 1])
+
+  return (
+    <AnimatedSectionWrapper ref={ref} className={className} id={id} style={{ opacity }}>
+      {children}
+    </AnimatedSectionWrapper>
+  )
+}
--- apps/example/components/Primitives/index.ts
+++ apps/example/components/Primitives/index.ts
@@ -1,3 +1,4 @@
 export * from './Headings'
 export * from './Badges'
 export * from './Message'
+export * from './Section'
