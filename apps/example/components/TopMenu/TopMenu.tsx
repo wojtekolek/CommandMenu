29421d2ca8e20f2862eb065496b2fe31d9ce4d40
wojtekolek <wojtek_olek@icloud.com>
2023-04-20 21:52:01 +0200

    Improve Topmenu styles

--- apps/example/components/TopMenu/TopMenu.tsx
+++ apps/example/components/TopMenu/TopMenu.tsx
@@ -8,7 +8,7 @@ import { Badges } from "components/Primitives";
 import Logo from "public/logo.svg";
 
 export const TopMenu: FunctionComponent = () => (
-  <div className="tablet:my-6 desktop:my-10 mx-8 my-4 flex items-center justify-between">
+  <div className="desktop:my-10 mx-8 my-8 flex items-center justify-between">
     <div className="flex items-center text-lg">
       <Image className="mr-2" alt="Logo" src={Logo} width={35} height={26} />
       Command Menu
