de592dad29b7dc7b100b7d78a35aca081b862e1c
wojtekolek <wojtek_olek@icloud.com>
2023-04-24 14:18:58 +0200

    Improve code snippets theme

--- apps/example/components/CodeSnippet/CodeSnippet.tsx
+++ apps/example/components/CodeSnippet/CodeSnippet.tsx
@@ -1,10 +1,11 @@
 import type { FunctionComponent } from "react";
 
-import { Light as SyntaxHighlighter } from "react-syntax-highlighter";
-import ts from "react-syntax-highlighter/dist/esm/languages/hljs/typescript";
-import { nord } from "react-syntax-highlighter/dist/esm/styles/hljs";
+import { PrismLight as SyntaxHighlighter } from "react-syntax-highlighter";
+import jsx from "react-syntax-highlighter/dist/esm/languages/prism/jsx";
 
-SyntaxHighlighter.registerLanguage("typescript", ts);
+import { theme } from "./theme";
+
+SyntaxHighlighter.registerLanguage("jsx", jsx);
 
 type CodeSnippetProps = {
   children: string;
@@ -12,17 +13,8 @@ type CodeSnippetProps = {
 
 export const CodeSnippet: FunctionComponent<CodeSnippetProps> = ({ children }) => (
   <div className="from-secondary-400/5 rounded-lg bg-gradient-to-br p-3">
-    <div className="bg-primary-950/20 m-0 overflow-x-auto rounded p-4 backdrop-blur-sm">
-      <SyntaxHighlighter
-        showLineNumbers
-        language="typescript"
-        style={nord}
-        wrapLongLines
-        customStyle={{
-          padding: 0,
-          background: "unset",
-        }}
-      >
+    <div className="bg-primary-950/20 m-0 rounded p-4 backdrop-blur-sm">
+      <SyntaxHighlighter showLineNumbers language="jsx" style={theme} wrapLines>
         {children}
       </SyntaxHighlighter>
     </div>
